0055e767caaef36d348e79c2246a5d058757de08
Peem Peeranat <peempeeranat@gmail.com>
2025-06-07 12:53:25 +0700

    change env

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ const db = mysql.createConnection({
 
 const initMySQL = async () => {
   connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'users',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
   });
 }
 
